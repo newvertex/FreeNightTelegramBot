579a86667cb36c82765c523da8985a5c866971ad
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2016-12-18 20:04:11 +0330

    Use multi-lang in MoviePoster templates

--- templates/movie-poster.js
+++ templates/movie-poster.js
@@ -1,69 +1,73 @@
 const Template = require('./template');
+const __ = require('multi-lang')('lang/movie-poster.lang.json');
 
 class MoviePoster extends Template {
-  constructor() {
+  constructor(lang = 'en') {
     super('Movie Poster', 'photo', [{
       name: 'photo',
       type: 'photo',
       multiple: false,
       required: true,
-      prompt: 'Upload a photo or give me the image link',
+      prompt: __('p-uploadPhoto', lang),
       value: []
     }, {
       name: 'movie-name',
       type: 'text',
       multiple: false,
       required: true,
-      prompt: 'What is the movie name?',
+      prompt: __('p-movieName', lang),
       value: []
     }, {
       name: 'movie-director',
       type: 'text',
       multiple: false,
       required: true,
-      prompt: 'Who is the director?',
+      prompt: __('p-movieDirector', lang),
       value: []
     }, {
       name: 'movie-stars',
       type: 'text',
       multiple: false,
       required: true,
-      prompt: 'Starring?',
+      prompt: __('p-starring', lang),
       value: []
     }, {
       name: 'movie-release-date',
       type: 'text',
       multiple: false,
       required: true,
-      prompt: 'Release dates?',
+      prompt: __('p-release', lang),
       value: []
     }, {
       name: 'hash-tag',
       type: 'text',
       multiple: false,
       required: false,
-      prompt: 'Hash tag?',
+      prompt: __('p-hashTag', lang),
       value: []
     }]);
 
   }
 
-  result() {
+  result(lang = 'en') {
     let photo = super.getFieldValue('photo')[0];
-
-    let title = `🎥Movie: ${super.getFieldValue('movie-name')[0] || '-'}`;
-    let director = `👤Director: ${super.getFieldValue('movie-director')[0] || '-'}`;
-    let starring = `👥Starring: ${super.getFieldValue('movie-stars')[0] || '-'}`;
-    let release = `📆Release date: ${super.getFieldValue('movie-release-date')[0] || '-'}`;
 
     let tag = super.getFieldValue('hash-tag')[0] || '-';
     tag = (tag !== '-') ? '\n' + tag.trim().split(' ').map(i => '#' + i).join(' ') : '';
+
+    let data = {
+      'name': super.getFieldValue('movie-name')[0] || '-',
+      'director': super.getFieldValue('movie-director')[0] || '-',
+      'starring': super.getFieldValue('movie-stars')[0] || '-',
+      'release': super.getFieldValue('movie-release-date')[0] || '-',
+      'tag': 'tag'
+    };
 
     return {
       type: 'photo',
       data: {
         photo: photo,
-        text: `${title}\n${director}\n${starring}\n${release}\n${tag}\n`
+        text: __('result', data, lang)
       }
     }
   }
