b548ffa2ecffc6272be00445b11c1e83667416db
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2016-12-18 20:08:11 +0330

    Use multi-lang, some refactor

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Telegraf = require('telegraf');
 const { memorySession } = require('telegraf');
 
+const __ = require('multi-lang')('lang/lang.json', 'en', false);
+
 let userManager = require('./user-manager');
 let imageUploader = require('./image-uploader');
 let shortLink = require('./shortlink');
@@ -16,6 +18,11 @@ bot.use(memorySession());
 // Use array to keep track of user on registration
 let registerSession = [];
 
+// Get user lang from user manager, use this to simple access
+function getLang(userId) {
+  return userManager.getLang(userId);
+}
+
 // Put user on registration mode and ask for confirmation
 bot.command('register', (ctx) => {
   if (typeof (ctx.message.chat.type) !== 'undefined' &&
@@ -27,7 +34,7 @@ bot.command('register', (ctx) => {
 
     registerSession[userId] = null;
 
-    ctx.reply(`I get your register request,\nSend below command on group or channel you want to register on it:\n/reg ${userId}\nor send /cancelReg to cancel registration!`);
+    ctx.reply(__('register-start', { userId }, getLang(userId)));
   }
 });
 
@@ -47,15 +54,15 @@ bot.hears(/^\/confirm (.+)$/, (ctx) => {
 
     if (userManager.addKey(user)) {
       ctx.telegram.sendMessage(userId,
-        `Registration confirmed, The new name is: ${chatKey}`);
+        __('register-confirm-success', { chatKey }, getLang(userId)));
 
       delete registerSession[userId];
     } else {
       ctx.telegram.sendMessage(userId,
-        `The entered name was exists, try another one\nPlease confirm it with a unique name (you can't change it anymore),\nTo confirm it just send /confirm your-unique-name\nor to cancel it just send /cancelReg.`);
+        __('register-confirm-failed', getLang(userId)));
     }
   } else {
-    ctx.reply(`You are not on registration!\nUse /help to read more.`);
+    ctx.reply(__('register-not-started', getLang(userId)));
   }
 });
 
@@ -65,7 +72,8 @@ bot.command('cancelReg', (ctx) => {
   if (userId in registerSession) {
     delete registerSession[userId];
 
-    bot.telegram.sendMessage(userId, 'Registration canceled!');
+    bot.telegram.sendMessage(userId,
+      __('register-cancel', getLang(userId)));
   }
 });
 
@@ -79,19 +87,34 @@ bot.command('start', (ctx) => {
 
   userManager.addUser(userId);
 
-  ctx.reply(`Welcome 😊`);
+  ctx.reply(__('welcome'));
+
+  //TODO: ask user about language and set the default one
+});
+
+bot.hears(/^\/setLang (.+)$/, (ctx) => {
+  let userId = ctx.message.from.id;
+  let lang = ctx.match[1];
+
+  if (lang === 'fa' || lang === 'en') {
+    userManager.setLang(userId, lang);
+    ctx.reply(__('setLang-success', lang))
+  } else {
+    ctx.reply(__('setLang-failed', getLang(userId)));
+  }
 });
 
 bot.hears(/^\/shortLink (.+)$/, (ctx) => {
   // Get url from bot command, if url start with http:// or https:// continue otherwise send back error!
   let linkUrl = ctx.match[1].startsWith('http://') || ctx.match[1].startsWith('https://') ? ctx.match[1] : null;
+  let userId = ctx.message.from.id;
 
   if (linkUrl) {
-    ctx.reply(`Link address received, Please wait...`);
+    ctx.reply(__('shortLink-received', getLang(userId)));
 
     shortLink(linkUrl)
       .then((result) => {
-        let answerMsg = `Short link address is:\n\n ${result.shortUrl}\n\nRequested link was: ${result.url}`;
+        let answerMsg = __('shortLink-success', { shortUrl: result.shortUrl, url: result.url }, getLang(userId));
 
         if (result.fileInfo) {
           answerMsg = `File info: ${result.fileInfo.name} - ${result.fileInfo.sizeInMB} \n\n` + answerMsg;
@@ -102,14 +125,14 @@ bot.hears(/^\/shortLink (.+)$/, (ctx) => {
       .catch((err) => {
         // Code 1 for server error and code 2 for link access error
         if (err.code === 1) {
-          ctx.reply(`${err.message}\n\nRequested link was: ${err.result.url}`);
+          ctx.reply(__('shortLink-error-1', { "errMessage": err.message, "url": err.result.url }, getLang(userId)));
         } else if (err.code === 2) {
-          ctx.reply(`${err.message}\n\nShort link address is:\n\n ${err.result.shortUrl}\n\nRequested link was: ${err.result.url}`)
+          ctx.reply(__('shortLink-error-2', { "errMessage": err.message, "shortUrl": err.result.shortUrl, "url": err.result.url }, getLang(userId)));
         }
       });
 
   } else {
-    ctx.reply(`Link address is not valid!\nPlease use an address that starts with 'http://' or 'https://'`);
+    ctx.reply(__('shortLink-failed', lang));
   }
 
 });
@@ -118,12 +141,13 @@ bot.on('photo', (ctx) => {
   if (ctx.message.chat.type === 'private') {
     // Get file_if of best quality version of photo from photo list
     let photoFileId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
+    let userId = ctx.message.from.id;
 
     if (ctx.session.state === 'new' && ctx.session.store.currentFieldType === 'photo') {
         ctx.session.store.answer(photoFileId);
         fillStore(ctx);
     } else {
-      ctx.reply(`Image received, Please wait...`);
+      ctx.reply(__('image-received', getLang(userId)));
 
       // Get file link from telegram server
       ctx.telegram.getFileLink(photoFileId)
@@ -131,13 +155,13 @@ bot.on('photo', (ctx) => {
           // Upload photo from telegram server to imgur and return the link to user chat page
           imageUploader.uploadIt(fileLink)
             .then((jsonResult) => {
-              return ctx.reply(`Image link is:\n ${jsonResult.data.link}`);
+              return ctx.reply(__('image-success', { 'link': jsonResult.data.link }, getLang(userId)));
             }).catch((err) => {
-              return ctx.reply(`Error on uploading image:\n ${err.message}\ntry again later`);
+              return ctx.reply(__('image-upload-error', { 'errMessage': err.message }, getLang(userId)));
             });
 
         }).catch((err) => {
-          return ctx.reply(`Error on get file link:\n ${err.message}\ntry again later`);
+          return ctx.reply(__('image-error', { 'errMessage': err.message }, getLang(userId)));
         });
     }
   }
@@ -145,22 +169,27 @@ bot.on('photo', (ctx) => {
 
 bot.hears(/\/new (.+)$/, (ctx) => {
   if (ctx.message.chat.type === 'private') {
-    let template = templateManager.selectTemplate(ctx.match[1]);
+    let userId = ctx.message.from.id;
+
+    let template = templateManager.selectTemplate(ctx.match[1], getLang(userId));
 
     if (template) {
       ctx.session.state = 'new';
       ctx.session.store = template;
 
-      ctx.reply(`New ${template.name} was created👍, Please answer the questions to fill the fields of new post`)
+      ctx.reply(__('newPost-start', { 'templateName': template.name }, getLang(userId)))
         .then(res => {
           fillStore(ctx);
         });
+    } else {
+      ctx.reply(__('newPost-failed', getLang(userId)));
     }
   }
 });
 
 function fillStore(ctx) {
   let store = ctx.session.store;
+  let userId = ctx.message.from.id;
 
   if (store) {
     let prompt = store.prompt();
@@ -168,7 +197,7 @@ function fillStore(ctx) {
       ctx.reply(prompt);
     } else {
       ctx.session.state = 'ready';
-      ctx.reply(`This post is Ready to send\nNow you can use /sendTo channelKey or /sendTo groupKey commands to send this post to channel or group\nOr /preview to see a preview of your post`);
+      ctx.reply(__('newPost-ready', getLang(userId)));
     }
   }
 }
@@ -180,7 +209,7 @@ bot.command('preview', (ctx) => {
 
       let userId = ctx.message.from.id;
       let userSignature = userManager.getSignature(userId)
-      let result = ctx.session.store.result();
+      let result = ctx.session.store.result(getLang(userId));
 
       ctx.reply(`Preview of ${ctx.session.store.name}`)
         .then(res => {
@@ -192,14 +221,14 @@ bot.command('preview', (ctx) => {
         })
         .then(res => {
           if (state === 'new') {
-            ctx.reply(`This post is not ready to send!\nPlease fill all the fields`)
+            ctx.reply(__('newPost-not-ready', getLang(userId)))
               .then(res => {
                 fillStore(ctx);
               });
           }
         })
         .catch(err => {
-          ctx.reply(`Noting to show!\nFirst fill some fields`)
+          ctx.reply(__('newPost-is-empty', getLang(userId)))
             .then(res => {
               fillStore(ctx);
             });
@@ -263,6 +292,18 @@ bot.hears(/\/sendTo (.+)$/, (ctx) => {
   }
 });
 
+// Register user and send request to confirm registration
+function registerUser(ctx, userId, chatId) {
+  if (userId in registerSession) {
+    registerSession[userId] = chatId;
+    ctx.telegram.sendMessage(userId,
+      __('register-request-confirm', getLang(userId)));
+  } else {
+    ctx.telegram.sendMessage(userId,
+      __('register-not-started', getLang(userId)));
+  }
+}
+
 bot.on('channel_post', (ctx) => {
   if (typeof (ctx.update.channel_post.text) !== 'undefined') {
     if (ctx.update.channel_post.text.startsWith('/reg ')) {
@@ -270,14 +311,7 @@ bot.on('channel_post', (ctx) => {
       let chatId = ctx.update.channel_post.chat.id;
       let userId = ctx.update.channel_post.text.split(' ')[1];
 
-      if (userId in registerSession) {
-        registerSession[userId] = chatId;
-        ctx.telegram.sendMessage(userId,
-          `Please confirm your registration with a unique name (you can't change it anymore),\nTo confirm it just send /confirm your-unique-name\nor to cancel it just send /cancel.`);
-      } else {
-        ctx.telegram.sendMessage(userId,
-          `You are not on registration!\nUse /help to read more.`);
-      }
+      registerUser(ctx, userId, chatId);
     }
   }
 });
@@ -291,14 +325,7 @@ bot.on('message', (ctx) => {
       let chatId = ctx.message.chat.id;
       let userId = ctx.message.text.split(' ')[1];
 
-      if (userId in registerSession) {
-        registerSession[userId] = chatId;
-        ctx.telegram.sendMessage(userId,
-          `Please confirm your registration with a unique name (you can't change it anymore),\nTo confirm it just send /confirm your-unique-name\nor to cancel it just send /cancel.`);
-      } else {
-        ctx.telegram.sendMessage(userId,
-          `You are not on registration!\nUse /help to read more.`);
-      }
+      registerUser(ctx, userId, chatId);
     }
   }
 
