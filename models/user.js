a310d6c043fc5394ed0498f769d626293825adab
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2017-04-09 22:22:28 +0430

    change user model

--- models/user.js
+++ models/user.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-let userSchema = new mongoose.Schema({
-  _id: String,
+const tuserSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
   keys: [{
     key: { type: String },
     kid: { type: String }
   }],
-  signature: { type: String, default: '' },
-  lang: { type: String, default: 'en' }
+  signature: { type: String, 'default': '' },
+  lang: { type: String, 'default': 'en' },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('tuser', tuserSchema);
